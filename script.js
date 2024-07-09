aea9ff6eba17fba445fc70b4edb13a5000593959
Your Name <you@example.com>
2024-07-10 02:30:20 +0300

    added fonts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('applyText').addEventListener('click', function() {
-    const textInput = document.getElementById('textInput').value;
-    const fontSelect = document.getElementById('fontSelect').value;
+    const textInput = document.getElementById('name').value;
+    const fontSelect = document.querySelector('input[name="font"]:checked').value;
     const textOverlay = document.getElementById('textOverlay');
     const textOverlaySecond = document.getElementById('textOverlaySecond');
 
