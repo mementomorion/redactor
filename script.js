61db2e1e197a42dcc6a8e7920a515c3b7c622394
Your Name <you@example.com>
2024-07-10 21:47:24 +0300

    full fix test mobile

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ document.getElementById('applyGlow').addEventListener('click', function() {
     const textOverlay = document.getElementById('textOverlay');
     const textOverlay2 = document.getElementById('textOverlaySecond');
     if (glowElement.style.display === 'none' || glowElement.style.display === '') {
-        // glowElement.style.display = 'block';
+        glowElement.style.display = 'block';
         textOverlay.style.textShadow = '0 0 19px #fff, 0 0 40px #fff, 0 0 80px #fff, 0 0 100px #fff';
         textOverlay2.style.textShadow = '0 0 4px #fff, 0 0 8px #fff, 0 0 16px #fff, 0 0 19px #fff, 0 0 40px #fff, 0 0 80px #fff, 0 0 100px #fff';
     } else {
-        // glowElement.style.display = 'none';
+        glowElement.style.display = 'none';
         textOverlay.style.textShadow = 'none';
         textOverlay2.style.textShadow = 'none';
     }
